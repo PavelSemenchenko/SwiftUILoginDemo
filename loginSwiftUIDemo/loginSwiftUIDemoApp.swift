b6e711f3856acd6542160de3940ac8ee92dda270
Leksovich <imagemakerspa@icloud.com>
2023-06-05 22:39:39 +0300

    Initial Commit

---
+++ loginSwiftUIDemo/loginSwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  loginSwiftUIDemoApp.swift
+//  loginSwiftUIDemo
+//
+//  Created by mac on 05.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct loginSwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
