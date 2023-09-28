2d7fefe1be46c16880a249d915a5d162229373e8
Leksovich <imagemakerspa@gmail.com>
2023-09-28 11:25:22 +0300

    try add visible messages

--- loginSwiftUIDemo/Screens/MessagesScreen.swift
+++ loginSwiftUIDemo/Screens/MessagesScreen.swift
@@ -6,20 +6,40 @@
 //
 
 import SwiftUI
+import Combine
 import FirebaseAuth
 import FirebaseFirestore
 import FirebaseFirestoreSwift
 import FirebaseFirestoreCombineSwift
 
 struct MessagesScreen: View {
-    @ObservedObject var messagesVM: MessagesVM = MessagesVM()
+    @StateObject var messagesVM: MessagesVM = MessagesVM()
+    @State var items: [Message] = []
     
     var body: some View {
         VStack {
             Text("Messages")
+                .onReceive(messagesVM.items) {
+                    items = $0
+                }
+            if (items.isEmpty) {
+                Text("No messages")
+            } else {
+                List(items) { item in
+                    if (item.sender == messagesVM.sender) {
+                        VStack(alignment: .trailing) {
+                            Text( item.text ?? "")
+                        }
+                    } else {
+                        VStack(aligment: .leading) {
+                            Text(item.text ?? "")
+                        }
+                    }
+                }
+            }
             Button("Send") {
                 Task {
-                    await messagesVM.send
+                    await messagesVM.send()
                 }
             }
         }
@@ -28,13 +48,33 @@ struct MessagesScreen: View {
 
 class MessagesVM: ObservableObject {
     
+    var sender: String {
+        return Auth.auth().currentUser!.uid
+    }
+    var recipient: String {
+        return "3Xvd2rbVdbf1OXONG0ykK90QCD42"
+    }
+    
+    lazy var items: AnyPublisher<[Message], Never> = {
+        Firestore.firestore().collection("messages")
+            .order(by: "created", descending: true)
+            .whereField("sender", isEqualTo: sender)
+            .whereField("recipient", isEqualTo: recipient)
+            .snapshotPublisher()
+            .map { $0.documents }
+            .map { $0.map { doc in try? doc.data(as: Message.self)}}
+            .map { $0.compactMap { c in c}}
+            .replaceError(with: [])
+            .eraseToAnyPublisher()
+    }()
+    
     @MainActor func send() async {
         guard let currentUserId = Auth.auth().currentUser?.uid else {
-            fatalError("need authentication")
+            return
         }
         
         let message = Message(sender: currentUserId,
-                              recipient: "NqK3JBgfLPUcsy6CtS6bu0sPdjA2",
+                              recipient: recipient,
                               read: false,
                               text: "Hello",
                               attachments: nil,
