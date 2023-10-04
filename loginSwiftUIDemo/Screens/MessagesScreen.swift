2e32cd0b9b964fd3b8623484fa781ef1229c906d
Leksovich <imagemakerspa@gmail.com>
2023-10-04 11:37:20 +0300

    добавлено отображение отправленных и полученных сообщений

--- loginSwiftUIDemo/Screens/MessagesScreen.swift
+++ loginSwiftUIDemo/Screens/MessagesScreen.swift
@@ -15,6 +15,7 @@ import FirebaseFirestoreCombineSwift
 struct MessagesScreen: View {
     @StateObject var messagesVM: MessagesVM = MessagesVM()
     @State var items: [Message] = []
+    @State var message: String = ""
     
     var body: some View {
         VStack {
@@ -46,27 +47,15 @@ struct MessagesScreen: View {
                     }
                     .frame(maxWidth: .infinity, alignment: item.sender == messagesVM.sender ? .trailing : .leading)
                 }
-                /*
-                List(items) { item in
-                    if (item.sender == messagesVM.sender) {
-                        HStack {
-                            Spacer()
-                            Text(item.text ?? "")
-                                
-                        }.frame(maxWidth: .infinity,
-                                alignment: .leading)
-                    } else {
-                        HStack {
-                            Text(item.text ?? "")
-                        }.frame(maxWidth: .infinity,
-                                alignment: .leading)
-                    }
-                }*/
             }
-            Button("Send") {
-                Task {
-                    await messagesVM.send()
-                }
+            HStack {
+                TextField("Type text", text: $message)
+                Button("Send") {
+                    Task {
+                        await messagesVM.send(message)
+                        message = ""
+                    }
+                }.disabled(message.isEmpty)
             }.padding()
         }
     }
@@ -76,36 +65,60 @@ class MessagesVM: ObservableObject {
     
     var sender: String {
         return Auth.auth().currentUser!.uid
+        //        2qohzoWVk9an4k6vg9DhhTbRTP23
     }
     var recipient: String {
-        return "3Xvd2rbVdbf1OXONG0ykK90QCD42"
+        return Auth.auth().currentUser!.uid != "3Xvd2rbVdbf1OXONG0ykK90QCD42" ?
+        "3Xvd2rbVdbf1OXONG0ykK90QCD42" : "2qohzoWVk9an4k6vg9DhhTbRTP23"
     }
     
     lazy var items: AnyPublisher<[Message], Never> = {
         Firestore.firestore().collection("messages")
             .order(by: "created", descending: true)
-            //.whereField("sender", isEqualTo: sender)
-            //.whereField("recipient", isEqualTo: recipient)
+            .whereFilter(Filter.andFilter([
+                Filter.orFilter([
+                    Filter.whereField("sender", isEqualTo: sender),
+                    Filter.whereField("sender", isEqualTo: recipient),
+                ]),
+                Filter.orFilter([
+                    Filter.whereField("recipient", isEqualTo: recipient),
+                    Filter.whereField("recipient", isEqualTo: sender)
+                ])
+            ]))
+        //.whereField("sender", isEqualTo: sender)
+        //.whereField("recipient", isEqualTo: recipient)
             .snapshotPublisher()
             .map { $0.documents }
             .map { $0.map { doc in try? doc.data(as: Message.self)}}
             .map { $0.compactMap { c in c}}
+            .map { $0.sorted { m1, m2 in
+                m1.created < m2.created
+            }}
             .replaceError(with: [])
             .eraseToAnyPublisher()
     }()
     
-    @MainActor func send() async {
+    @MainActor func send(_ text: String) async {
         guard let currentUserId = Auth.auth().currentUser?.uid else {
             return
         }
         
         let message = Message(sender: currentUserId,
-                              recipient: "3Xvd2rbVdbf1OXONG0ykK90QCD42",
+                              recipient: recipient,
                               read: false,
-                              text: "Hello",
+                              text: text,
                               attachments: nil,
                               created: Date())
         let _ = Firestore.firestore().collection("messages").addDocument(from: message)
+        /*
+        let message1 = Message(sender: "3Xvd2rbVdbf1OXONG0ykK90QCD42",
+                               recipient: currentUserId,
+                               read: false,
+                               text: "\(text) back",
+                               attachments: nil,
+                               created: Date())
+        let _ = Firestore.firestore().collection("messages").addDocument(from: message1)
+         */
     }
 }
 
