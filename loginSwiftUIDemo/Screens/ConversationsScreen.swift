56b600617a6720789d0f8b463812ea3e90bbad4a
Pavlo Semenchenko <imagemakerspa@icloud.com>
2023-10-08 17:48:16 +0300

    ошибка - смс отображаются только после отправки нового сообщения

---
+++ loginSwiftUIDemo/Screens/ConversationsScreen.swift
@@ -0,0 +1,18 @@
+//
+//  ConversationsScreen.swift
+//  loginSwiftUIDemo
+//
+//  Created by Pavel Semenchenko on 08.10.2023.
+//
+
+import SwiftUI
+
+struct ConversationsScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ConversationsScreen()
+}
